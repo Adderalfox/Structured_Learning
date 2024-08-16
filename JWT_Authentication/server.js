c3dde552d9c079515fc580fa1d81e10b2e1bfff1
Adderalfox <105387879+Adderalfox@users.noreply.github.com>
2024-08-17 01:39:11 +0530

    Add files via upload

---
+++ JWT_Authentication/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const express = require("express")
+const app = express()
+
+const jwt = require("jsonwebtoken")
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'Kyle',
+        title: 'Post 1'
+    },
+    {
+        username: 'Tim',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username == req.user.name))
+
+});
+
+app.post('/login', (req, res) => {
+
+    const username = req.body.username
+    const user = { name: username }
+    console.log(req.query)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader 
+    try {
+        if (token == null) return res.sendStatus(401)
+        const decode =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        req.user = decode
+        next()
+    }
+    catch (err) {
+        return res.sendStatus(404)
+    }
+}
+
+app.listen(3000)
