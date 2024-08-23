b873da9d8cb01adbc6d3038e115e870c3ecc3fa8
Adderalfox <105387879+Adderalfox@users.noreply.github.com>
2024-08-23 15:50:43 +0530

    Update server.js
    updated with es-module

--- JWT_Authentication/server.js
+++ JWT_Authentication/server.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+import dotenv from "dotenv";
+import express from "express";
+import jwt from "jsonwebtoken";
+const app = express();
 
-const express = require("express")
-const app = express()
-
-const jwt = require("jsonwebtoken")
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
 
 const posts = [
     {
@@ -18,22 +18,22 @@ const posts = [
 ]
 
 app.get('/posts', authenticateToken, (req, res) => {
-    res.json(posts.filter(post => post.username == req.user.name))
+    res.json(posts.filter(post => post.username == req.user.name));
 
 });
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader 
+    const authHeader = req.headers['authorization'];
+    const token = authHeader; 
     try {
-        if (token == null) return res.sendStatus(401)
-        const decode =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        req.user = decode
-        next()
+        if (token == null) return res.sendStatus(401);
+        const decode =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        req.user = decode;
+        next();
     }
     catch (err) {
-        return res.sendStatus(404)
+        return res.sendStatus(404);
     }
 }
 
-app.listen(3000)
+app.listen(3000);
